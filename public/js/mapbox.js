926958c8823afada847eeaf8f998905c7cf109d0
Paul Miller <pmillerk88@gmail.com>
2016-08-29 12:07:35 -0400

    Removed return

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -11,50 +11,52 @@ var map = new mapboxgl.Map({
 });
 
 var chapters = {
-'home': {
-    bearing: 27,
-    center: [-77.321264, 38.943057],
-    zoom: 15.5
-},
-'listing1': {
-    duration: 6000,
-    center: [-77.321264, 39.943057],
-    bearing: 150,
-    zoom: 15
-   },
-'listing2': {
-    bearing: 90,
-    center: [-77.321264, 40.943057],
-    zoom: 13,
-    speed: 0.6
-},
-'listing3': {
-    bearing: 90,
-    center: [-77.321264, 41.943057],
-    zoom: 12.3
-},
-'listing4': {
-    bearing: 45,
-    center: [-77.321264, 42.943057],
-    zoom: 15.3,
-    speed: 0.5
-},
-'listing5': {
-    bearing: 180,
-    center: [-77.321264, 43.943057],
-    zoom: 12.3
-},
-'listing6': {
-    bearing: 90,
-    center: [-77.321264, 44.943057],
-    zoom: 17.3
-},
-'listing7': {
-    bearing: 90,
-    center: [-77.321264, 45.943057],
-    zoom: 14.3
-}
+  'home': {
+      bearing: 27,
+      center: [-77.321264, 38.943057],
+      zoom: 15.5
+  },
+  'listing1': {
+      duration: 6000,
+      center: [-77.321264, 39.943057],
+      bearing: 150,
+      zoom: 15
+     },
+  'listing2': {
+      bearing: 90,
+      center: [-77.321264, 40.943057],
+      zoom: 13,
+      speed: 0.6
+  },
+  'listing3': {
+      bearing: 90,
+      center: [-77.321264, 41.943057],
+      zoom: 12.3
+  },
+  'listing4': {
+      bearing: 45,
+      center: [-77.321264, 42.943057],
+      zoom: 15.3,
+      speed: 0.5
+  },
+  'listing5': {
+      bearing: 180,
+      center: [-77.321264, 43.943057],
+      zoom: 12.3
+  },
+  'listing6': {
+      bearing: 90,
+      center: [-77.321264, 44.943057],
+      zoom: 17.3
+  },
+  'listing7': {
+      bearing: 90,
+      center: [-77.321264, 45.943057],
+      zoom: 14.3
+  }
 };
+
+console.log("chapters initialized");
 
 // map.addControl(new mapboxgl.Geocoder());
 
@@ -62,8 +64,10 @@ var chapters = {
 ////////// Map Reposition w/ Chapter Tracker /////////////
 //////////////////////////////////////////////////////////
 
+console.log("pre scroll event");
 // On every scroll event, check which element is on screen
 window.addEventListener('scroll', function(e) {
+  console.log("scroll event started");
   var chapterNames = Object.keys(chapters);
   for (var i = 0; i < chapterNames.length; i++) {
       var chapterName = chapterNames[i];
@@ -73,6 +77,7 @@ window.addEventListener('scroll', function(e) {
       }
   }
 });
+console.log("post scroll event");
 
 
 // Chapter Tracker
@@ -94,6 +99,6 @@ function isElementOnScreen(id) {
   var flyby = document.getElementsByClassName("flyby")[0];
 
   return bounds.top < flyby.innerHeight && bounds.bottom > 0;
-  console.log(return bounds.top < flyby.innerHeight && bounds.bottom > 0);
+  console.log(bounds.top < flyby.innerHeight && bounds.bottom > 0);
 }
 })();
